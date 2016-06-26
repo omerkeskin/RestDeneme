6c5d75e5dda08612a1ef2871ffe9c374424bf5f9
Omer Keskin <omer.keskin.deu@hotmail.com>
2016-06-26 16:26:04 +0300

    -Put eklendi.

--- messenger/src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -2,8 +2,12 @@ package org.koushik.javabrains.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,9 +19,34 @@ public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId")long id, Message message){
+		message.setId(id);
+		return messageService.addMessage(message);
+	}
+	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
 	}
+	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId")long id){
+		return messageService.getMessage(id);
+	}
+	
+	
 }
